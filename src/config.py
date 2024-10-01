93c1bb7468bc7ce6a92aaf385e038b27c1dca800
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2024-09-30 20:55:44 -0500

    read mail configurations from env

--- src/config.py
+++ src/config.py
@@ -8,11 +8,25 @@ class Settings(BaseSettings):
     database_name: str
     database_port: str
     secret_key: str
-    security_password_salt: str
+    security_salt: str
     algorithm: str
     access_token_expire_minutes: int
+    domain: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    mail_username: str
+    mail_password: str
+    mail_server: str
+    mail_port: int
+    mail_from: str
+    mail_from_name: str
+    mail_starttls: bool
+    mail_ssl_tls: bool
+    use_credentials: bool
+    validate_certs: bool
+
+    app_name: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 settings = Settings()
