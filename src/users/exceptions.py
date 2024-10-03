54111f8ee056ea721131ee3b813d94c5c82a2ebe
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2024-10-02 20:55:02 -0500

    exception classes for user module

---
+++ src/users/exceptions.py
@@ -0,0 +1,26 @@
+from fastapi import status
+
+from src.exceptions import BadRequest, DetailedHTTPException
+
+
+class EmailTakenException(BadRequest):
+    DETAIL = "Email already exists."
+
+
+class UsernameTakenException(BadRequest):
+    DETAIL = "Username already exists."
+
+
+class VerificationLinkExpired(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_410_GONE
+    DETAIL = "Verification Link Expired"
+
+
+class BadSignature(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_401_UNAUTHORIZED
+    DETAIL = "Invalid Verification Token"
+
+
+class UserNotFound(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_404_NOT_FOUND
+    DETAIL = "User Not Found"
