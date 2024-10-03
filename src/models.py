906e17433ba3f0062b6b78cfb18f43f2a7a1b139
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2024-10-02 20:57:25 -0500

    user model for database table

---
+++ src/models.py
@@ -0,0 +1,34 @@
+import uuid
+
+from sqlalchemy import (
+    Boolean,
+    Column,
+    Date,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    Text,
+    func,
+)
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from src.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    bio = Column(Text, nullable=True)
+    location = Column(String(100), nullable=True)
+    profile_image_url = Column(Text, nullable=True)
+    header_image_url = Column(Text, nullable=True)
+    birth_date = Column(Date, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    is_verified = Column(Boolean, default=False)
