a2cad97b1df75ab9d02ae4533bd6974ccc54fef1
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2024-10-18 22:00:18 -0500

    add follow table for follow functionality

--- src/models.py
+++ src/models.py
@@ -21,6 +21,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    full_name = Column(String(100), nullable=False)
     username = Column(String(50), unique=True, nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
@@ -31,9 +32,23 @@ class User(Base):
     birth_date = Column(Date, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    is_verified = Column(Boolean, default=False)
+    verified_on = Column(DateTime(timezone=True), nullable=True)
 
     tweets = relationship("Tweet", back_populates="user", cascade="all, delete-orphan")
+
+    following = relationship(
+        "Follow",
+        backref="follower",
+        foreign_keys="[Follow.follower_id]",
+        cascade="all, delete-orphan",
+    )
+
+    followers = relationship(
+        "Follow",
+        backref="followed",
+        foreign_keys="[Follow.followed_id]",
+        cascade="all, delete-orphan",
+    )
 
 
 class Tweet(Base):
@@ -53,3 +68,11 @@ class Tweet(Base):
         backref="parent_tweet",
         cascade="all",
     )
+
+
+class Follow(Base):
+    __tablename__ = "follows"
+
+    follower_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), primary_key=True)
+    followed_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), primary_key=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
