aadf0641be3c1ce04cb09daa2d7663f2081a42b7
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2024-10-21 20:20:06 -0500

    return follow stats from get user apis

--- src/users/routers.py
+++ src/users/routers.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter, BackgroundTasks, Depends, status
+import datetime
+import uuid
+from typing import Annotated
+
+from fastapi import APIRouter, BackgroundTasks, Depends, File, UploadFile, status
 from fastapi.responses import JSONResponse
+from pydantic import StringConstraints
 from sqlalchemy.orm import Session
 
+from src import models
 from src.database import get_db
 from src.dependencies import get_current_user
-from src.models import User
 from src.users import schemas, service
 
 router = APIRouter(prefix="/users", tags=["Users"])
@@ -41,10 +46,20 @@ async def create_user_account(
 
 
 @router.put(
-    "", status_code=status.HTTP_200_OK, response_model=schemas.CreateUserResponse
+    "", status_code=status.HTTP_200_OK, response_model=schemas.UpdateUserResponse
 )
 async def update_user_account(
-    updated_details: schemas.UserUpdate,
+    username: Annotated[str, StringConstraints(min_length=3, max_length=50)] | None = (
+        None
+    ),
+    full_name: (
+        Annotated[str, StringConstraints(min_length=1, max_length=100)] | None
+    ) = None,
+    bio: Annotated[str, StringConstraints(max_length=255)] | None = None,
+    location: Annotated[str, StringConstraints(max_length=100)] | None = None,
+    birth_date: datetime.date | None = None,
+    profile_image: Annotated[UploadFile, File] | None = None,
+    header_image: Annotated[UploadFile, File] | None = None,
     current_user=Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
@@ -68,7 +83,15 @@ async def update_user_account(
     - 400 Bad Request: If a username conflict occurs.
     """
     return await service.update_user_details(
-        updated_details, current_user_id=current_user.id, db=db
+        username,
+        full_name,
+        bio,
+        location,
+        birth_date,
+        profile_image,
+        header_image,
+        current_user.id,
+        db,
     )
 
 
@@ -91,8 +114,51 @@ async def verify_user(
         UserNotFound: If no user is found with the email decoded from the token.
 
     Notes:
-        - The user's account is activated by setting the `is_verified` flag to True.
+        - The user's account is activated by setting the `verified_at` field to the verification date.
         - After activation, a confirmation email is sent asynchronously.
     """
     await service.activate_user_account(token, db, background_tasks)
     return JSONResponse({"message": "Account is activated successfully."})
+
+
+@router.get(
+    "/me",
+    status_code=status.HTTP_200_OK,
+    response_model=schemas.CurrentUserDetailsResponse,
+)
+async def get_current_user_details(
+    current_user: models.User = Depends(get_current_user), db: Session = Depends(get_db)
+):
+    """
+    Get the details of the currently authenticated user.
+
+    Args:
+        current_user (models.User): The user retrieved from the authentication token.
+        db (Session): The database session.
+
+    Returns:
+        JSON response containing the user's details.
+    """
+
+    # Todo: Add logic to get the number of following and followers for the current user.
+    follow_stats = await service.get_follow_stats(current_user.id, db)
+    user_details = schemas.CurrentUserDetailsResponse.model_validate(
+        current_user
+    ).model_dump()
+    return dict(user_details, **follow_stats)
+
+
+@router.get(
+    "/{user_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=schemas.UserDetailsResponse,
+)
+async def get_user_details(
+    user_id: uuid.UUID,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_user),
+):
+    follow_stats = await service.get_follow_stats(user_id=user_id, db=db)
+    user_obj, is_followed = await service.get_user_details(user_id, current_user.id, db)
+    user_details = schemas.UserDetailsResponse.model_validate(user_obj).model_dump()
+    return dict(user_details, is_followed=is_followed, **follow_stats)
