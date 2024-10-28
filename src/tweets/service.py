57cda66b7e3404e45aabb27a95499ff567479357
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2024-10-28 18:33:07 -0500

    add services to get user replies and tweets

--- src/tweets/service.py
+++ src/tweets/service.py
@@ -2,12 +2,13 @@ import uuid
 
 from fastapi import UploadFile
 from sqlalchemy import desc, func
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 
 from src import models
 from src.tweets import schemas
 from src.tweets.exceptions import InvaildParentTweet, MediaUploadError, TweetNotFound
 from src.tweets.utils import save_tweet_media
+from src.users.exceptions import UserNotFound
 
 
 async def create_new_tweet(
@@ -38,17 +39,6 @@ async def create_new_tweet(
     db.commit()
     db.refresh(new_tweet)
     return new_tweet
-
-
-# async def get_all_tweets(offset, limit, tweeted_after, tweeted_before, db):
-#     query = db.query(models.Tweet)
-#     if tweeted_after:
-#         query = query.filter(models.Tweet.created_at > tweeted_after)
-#     if tweeted_before:
-#         query = query.filter(models.Tweet.created_at < tweeted_before)
-#     query = query.order_by(desc(models.Tweet.created_at))
-#     tweets = query.offset(offset).limit(limit).all()
-#     return {"tweets": tweets}
 
 
 async def delete_tweet(tweet_id, current_user_id, db):
@@ -170,3 +160,124 @@ async def get_tweet_details(
         reply_ids=[reply_id for (reply_id,) in reply_ids],
     )
     return response
+
+
+async def get_user_tweets(user_id: uuid.UUID, skip: int, limit: int, db: Session):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise UserNotFound
+
+    # Get user's original tweets (non-replies)
+    tweets_query = (
+        db.query(models.Tweet)
+        .filter(models.Tweet.user_id == user_id, models.Tweet.parent_tweet_id.is_(None))
+        .options(
+            joinedload(models.Tweet.user),
+            joinedload(models.Tweet.likes),
+            joinedload(models.Tweet.retweets),
+            joinedload(models.Tweet.replies),
+        )
+    )
+
+    # Get user's retweets
+    retweets_query = (
+        db.query(models.Tweet)
+        .join(models.Retweet)
+        .filter(models.Retweet.user_id == user_id)
+        .options(
+            joinedload(models.Tweet.user),
+            joinedload(models.Tweet.likes),
+            joinedload(models.Tweet.retweets),
+            joinedload(models.Tweet.replies),
+        )
+    )
+
+    # Combine and order results
+    combined_results = []
+
+    # Add original tweets
+    tweets = tweets_query.offset(skip).limit(limit).all()
+    for tweet in tweets:
+        tweet_response = {
+            "id": tweet.id,
+            "content": tweet.content,
+            "media_url": tweet.media_url,
+            "created_at": tweet.created_at,
+            "user": tweet.user,
+            "likes_count": len(tweet.likes),
+            "retweets_count": len(tweet.retweets),
+            "replies_count": len(tweet.replies),
+            "is_retweet": False,
+        }
+        combined_results.append(tweet_response)
+
+    # Add retweets
+    retweets = retweets_query.offset(skip).limit(limit).all()
+    for tweet in retweets:
+        retweet = (
+            db.query(models.Retweet)
+            .filter(
+                models.Retweet.tweet_id == tweet.id, models.Retweet.user_id == user_id
+            )
+            .first()
+        )
+
+        retweet_response = {
+            "id": tweet.id,
+            "content": tweet.content,
+            "media_url": tweet.media_url,
+            "created_at": retweet.created_at,  # Use retweet timestamp
+            "user": tweet.user,  # Original tweet creator
+            "retweeted_by": retweet.user,  # User who retweeted
+            "likes_count": len(tweet.likes),
+            "retweets_count": len(tweet.retweets),
+            "replies_count": len(tweet.replies),
+            "is_retweet": True,
+        }
+        combined_results.append(retweet_response)
+
+    # Sort combined results by created_at
+    combined_results.sort(key=lambda x: x["created_at"], reverse=True)
+    return {"tweets": combined_results}
+
+
+async def get_user_replies(user_id: uuid.UUID, skip: int, limit: int, db: Session):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise UserNotFound
+
+    # Get user's replies
+    replies = (
+        db.query(models.Tweet)
+        .filter(
+            models.Tweet.user_id == user_id, models.Tweet.parent_tweet_id.isnot(None)
+        )
+        .options(
+            joinedload(models.Tweet.user),
+            joinedload(models.Tweet.parent_tweet).joinedload(models.Tweet.user),
+            joinedload(models.Tweet.likes),
+            joinedload(models.Tweet.retweets),
+            joinedload(models.Tweet.replies),
+        )
+        .order_by(models.Tweet.created_at.desc())
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
+
+    # Prepare response
+    replies_response = []
+    for reply in replies:
+        reply_data = {
+            "id": reply.id,
+            "content": reply.content,
+            "media_url": reply.media_url,
+            "created_at": reply.created_at,
+            "user": reply.user,
+            "likes_count": len(reply.likes),
+            "retweets_count": len(reply.retweets),
+            "replies_count": len(reply.replies),
+            "parent_tweet": reply.parent_tweet,
+        }
+        replies_response.append(reply_data)
+    return {"replies": replies_response}
