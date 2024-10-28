049fbb73548c0663b6e1a1b6c43bc5802cf457b4
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2024-10-28 00:04:25 -0500

    add schema for follow suggestions

--- src/follow/schemas.py
+++ src/follow/schemas.py
@@ -1,4 +1,5 @@
 from typing import List
+from uuid import UUID
 
 from pydantic import BaseModel, ConfigDict
 
@@ -19,5 +20,15 @@ class FollowersDetailsResponse(BaseModel):
 
 class FollowingDetailsResponse(BaseModel):
     following: List[FollowUserDetails]
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class FollowSuggestionsResponse(BaseModel):
+    id: UUID
+    full_name: str
+    username: str
+    profile_image_url: str | None
+    is_followed: bool = False
 
     model_config = ConfigDict(from_attributes=True)
